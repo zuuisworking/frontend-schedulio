528adca09a982c142a696a0e2f6f47f3a68161fa
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-07-11 09:56:33 +0700

    refactor: restructure API routing by moving index.php to api directory

--- api/index.php
+++ api/index.php
@@ -9,7 +9,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($uri) {
     case '/':
     case '/login':
-        require_once __DIR__ . '/app/controllers/auth_controller.php';
+        require_once __DIR__ . '/../app/controllers/auth_controller.php';
         $controller = new AuthController();
         if ($method === 'POST') {
             $controller->processLogin();
@@ -19,19 +19,19 @@ switch ($uri) {
         break;
 
     case '/logout':
-        require_once __DIR__ . '/app/controllers/auth_controller.php';
+        require_once __DIR__ . '/../app/controllers/auth_controller.php';
         $controller = new AuthController();
         $controller->logout();
         break;
 
     case '/dashboard':
-        require_once __DIR__ . '/app/controllers/dashboard_controller.php';
+        require_once __DIR__ . '/../app/controllers/dashboard_controller.php';
         $controller = new DashboardController();
         $controller->index();
         break;
 
     case '/tasks':
-        require_once __DIR__ . '/app/controllers/task_controller.php';
+        require_once __DIR__ . '/../app/controllers/task_controller.php';
         $controller = new TaskController();
         if ($method === 'POST') {
             $controller->store();
@@ -41,13 +41,13 @@ switch ($uri) {
         break;
 
     case '/tasks/create':
-        require_once __DIR__ . '/app/controllers/task_controller.php';
+        require_once __DIR__ . '/../app/controllers/task_controller.php';
         $controller = new TaskController();
         $controller->create();
         break;
 
     case '/tasks/delete':
-        require_once __DIR__ . '/app/controllers/task_controller.php';
+        require_once __DIR__ . '/../app/controllers/task_controller.php';
         $controller = new TaskController();
         $id = $_GET['id'] ?? null;
         if ($id) {
@@ -58,7 +58,7 @@ switch ($uri) {
         break;
 
     case '/schedule':
-        require_once __DIR__ . '/app/controllers/schedule_controller.php';
+        require_once __DIR__ . '/../app/controllers/schedule_controller.php';
         $controller = new ScheduleController();
         $controller->index();
         break;
